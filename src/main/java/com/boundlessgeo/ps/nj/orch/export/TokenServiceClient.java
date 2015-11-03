22d025fd864ffcc07047d214d2e04253117e0552
Matt Kruszewski <mkruszewski@boundlessgeo.com>
2015-11-02 21:41:27 -0600

    Added token isValid method to TokenServiceClient

--- src/main/java/com/boundlessgeo/ps/nj/orch/export/TokenServiceClient.java
+++ src/main/java/com/boundlessgeo/ps/nj/orch/export/TokenServiceClient.java
@@ -1,6 +1,7 @@
 package com.boundlessgeo.ps.nj.orch.export;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,4 +13,8 @@ public interface TokenServiceClient {
 	public String get(@RequestParam("user") String user,
 			@RequestParam("password") String password);
 
+	@RequestMapping(method = RequestMethod.GET,
+					 value = "/FlightOps_Token/token/isValid/{token}")
+	public String isValid(@PathVariable("token") String token);
+	
 }
